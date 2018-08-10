54eb10ccbc50c34bcfb7d5f6c5e3b83ba45197c4
lbassett <liam.f.bassett@gmail.com>
2018-08-10 16:49:35 -0400

    Added winner to BL Status

--- Battlelines/BattleLineStatus.py
+++ Battlelines/BattleLineStatus.py
@@ -5,9 +5,11 @@ import GlobalBattleLineVals
 class BLstatus:
 
 	def __init__(self):
-		self.status = [[None,None,None,None,None,None,None]]*9
+		self.status = [[None,None,None,None,None,None,None]]*11 + [[None]]
 		self.hands = self.initializehands()
 		self.flags = [0,0,0,0,0,0,0,0,0]
+		self.ones = 0
+		self.twos = 0
 
 
 	def initializehands(self):
@@ -21,5 +23,34 @@ class BLstatus:
 			newlist += x.P1Hand
 			newlist += [x.whowins()]
 			newlist += x.P2Hand
-			self.hands[counter] = newlist
+			self.status[counter] = newlist
 			counter+= 1
+			self.status[11] = self.winner()
+
+	def winner(self):
+		if self.ones >=5:
+			return(1)
+		elif self.twos >= 5:
+			return(2)
+		else:
+			one = 0
+			two = 0
+			for x in self.flags:
+				if x == 1:
+					one += 1
+					two = 0
+				elif x == 2:
+					two += 1
+					one = 0
+				elif x == 0:
+					two = 0
+					one = 0
+				pass
+				if one == 3:
+					return(1)
+				elif two == 3:
+					return(2)
+
+
+
+
